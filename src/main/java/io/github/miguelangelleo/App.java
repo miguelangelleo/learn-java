a9ce855f71c2845264474178cf64825e974c2fd6
Miguelangel Leo <183713908+miguelangelleo@users.noreply.github.com>
2024-10-03 12:43:39 +0100

    init

---
+++ src/main/java/io/github/miguelangelleo/App.java
@@ -0,0 +1,13 @@
+package io.github.miguelangelleo;
+
+public class App {
+    /**
+     * TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+     * click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+     * 
+     * @param args The command line args
+     */
+    public static void main(String[] args) {
+        System.out.printf("Hello and welcome!");
+    }
+}
